d213b16388fed237c3e6359405701461311a4d69
Ashley Heestand <heestand.ashley@gmail.com>
2019-07-30 12:56:17 -0500

    more store

--- client/store.js
+++ client/store.js
@@ -1,3 +1,7 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import thunk from 'redux-thunk';
+
 //Initial State
 const initialState = {
   messsages: [],
@@ -22,3 +26,6 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+export default store;
