d5af94071e27e313c4ee665baa9c2227e76571f0
Ashley Heestand <heestand.ashley@gmail.com>
2019-08-13 15:26:09 -0500

    channels

--- client/components/ChannelList.js
+++ client/components/ChannelList.js
@@ -1,43 +1,66 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import store, { fetchChannels } from '../store';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 // These values are all hardcoded...for now!
 // Soon, we'll fetch them from the server!
-const RANDOM_CHANNEL = '/channels/1';
-const GENERAL_CHANNEL = '/channels/2';
-const DOGS_CHANNEL = '/channels/3';
-const LUNCH_CHANNEL = '/channels/4';
+// const RANDOM_CHANNEL = '/channels/1';
+// const GENERAL_CHANNEL = '/channels/2';
+// const DOGS_CHANNEL = '/channels/3';
+// const LUNCH_CHANNEL = '/channels/4';
 
-export default class ChannelList extends Component {
+export class ChannelList extends Component {
+  constructor() {
+    super();
+    this.state = { channels: [], messages: [] };
+  }
+  async componentDidMount() {
+    await this.props.fetchInitialChannels();
+    this.setState(store.getState);
+  }
 
-  render () {
+  render() {
+    const channels = this.state.channels;
+    const messages = this.state.messages;
+    console.log(this.state);
     return (
       <ul>
-        <li>
-          <NavLink to={RANDOM_CHANNEL} activeClassName="active">
-            <span># really_random</span>
-            <span className="badge">0</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={GENERAL_CHANNEL} activeClassName="active">
-            <span># generally_speaking</span>
-            <span className="badge">0</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={DOGS_CHANNEL} activeClassName="active">
-            <span># dogs_of_fullstack</span>
-            <span className="badge">0</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={LUNCH_CHANNEL} activeClassName="active">
-            <span># lunch_planning</span>
-            <span className="badge">0</span>
-          </NavLink>
-        </li>
+        {channels.map(channel => (
+          <li key={channel.id}>
+            <NavLink to={`/channels/${channel.id}`} activeClassName="active">
+              <span>{channel.name}</span>
+              <span className="badge">
+                {
+                  messages.filter(message => message.channelId === channel.id)
+                    .length
+                }
+              </span>
+            </NavLink>
+          </li>
+        ))}
       </ul>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    channels: state.channels,
+    messages: state.messages,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchInitialChannels: () => dispatch(fetchChannels()),
+  };
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(ChannelList),
+);
--- client/store.js
+++ client/store.js
@@ -5,17 +5,25 @@ import axios from 'axios';
 
 //Initial State
 export const initialState = {
-  messsages: [],
+  messages: [],
+  channels: [],
 };
 
 //Action Types
 const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER';
+const GOT_CHANNELS_FROM_SERVER = 'GOT_CHANNELS_FROM_SERVER';
 
 //Action Creators
 //Messages is the payload of the action
 export const gotMessagesFromServer = messages => ({
   type: GOT_MESSAGES_FROM_SERVER,
   messages,
+});
+
+export const gotChannelsFromServer = (messages, channels) => ({
+  type: GOT_CHANNELS_FROM_SERVER,
+  messages,
+  channels,
 });
 
 export const fetchMessages = () => {
@@ -27,11 +35,27 @@ export const fetchMessages = () => {
   };
 };
 
+export const fetchChannels = () => {
+  return async dispatch => {
+    const response = await axios.all([
+      axios.get('/api/messages'),
+      axios.get('/api/channels'),
+    ]);
+    const messages = response[0].data;
+    const channels = response[1].data;
+    const action = gotChannelsFromServer(messages, channels);
+    dispatch(action);
+  };
+};
+
 //Reducer
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case GOT_MESSAGES_FROM_SERVER:
       return { ...state, messages: action.messages };
+    case GOT_CHANNELS_FROM_SERVER:
+      let intermediary = { ...state, messages: action.messages };
+      return { ...intermediary, channels: action.channels };
     default:
       return state;
   }
